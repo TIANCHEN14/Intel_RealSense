864c11161fbddf321b25ff193ab8a30d872b3426
TIANCHEN14 <109315683+TIANCHEN14@users.noreply.github.com>
2023-11-21 18:09:39 -0500

    This commit add jupyter notebook in this repo

---
+++ Realsense Playground/python_file/align_and_background_removal.py
@@ -0,0 +1,23 @@
+#####################################################
+##              Align Depth to Color               ##
+#####################################################
+
+# First import the library
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+
+# Create a pipeline
+pipeline = rs.pipeline()
+
+# Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
