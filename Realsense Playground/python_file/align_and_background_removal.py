d4971cb01103c755bb7745e69f068f1f49dd0869
TIANCHEN14 <109315683+TIANCHEN14@users.noreply.github.com>
2023-11-21 20:54:56 -0500

    Update align_and_background_removal.py

--- Realsense Playground/python_file/align_and_background_removal.py
+++ Realsense Playground/python_file/align_and_background_removal.py
@@ -21,3 +21,5 @@ pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+print("Device : ", device)
