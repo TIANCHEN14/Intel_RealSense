68a9933e5173d4b0afaae27b139f8d531f251a17
TIANCHEN14 <109315683+TIANCHEN14@users.noreply.github.com>
2024-04-03 17:19:30 -0400

    Ray Casting initial Commit
    Create notebook to test ray casting algorithm to make sure it works, Test to implement into the library not working yet. Need to refine

--- Realsense Playground/python_file/simple_marker_playground/D405_lib.py
+++ Realsense Playground/python_file/simple_marker_playground/D405_lib.py
@@ -9,16 +9,24 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import matplotlib as plt
+import sys
 
 # Marker class to help with processing information of the image
 class marker:
     
+    ###############################################
+    ###             Constructors                ###
+    ###############################################
+        ################################################################################################################################################
     # initialization of the image sensors and store all the information
     # in self object it contain following object
     # self{
     #       marker_dict         Type : python dict
     #       color_image         Type : np.array
     #       depth_image         Type : np.array
+    #       depth_colormap      Type : np.array
+    #       text_image          Type : np.array
+    #       mask_image          Type : np.array
     #       pipeline            Type : rs object
     #       align               Type : rs object
     #}
@@ -67,8 +75,12 @@ class marker:
         self.depth_colormap = None
         self.text_image = None
         self.mask_image = None
-    
 
+    ##################################################
+    ###             Public Function                ###
+    ##################################################
+        ################################################################################################################################################
+    
     # generate color frame and depth frame
     def get_frames(self):
 
@@ -131,19 +143,9 @@ class marker:
                 temp_dict['corner_3'] = marker_corners[index][0][3]
                 self.marker_dict[int(id)] = temp_dict
 
-
-
-                #self.marker_dict[int(id)]['depth'] = avg_depth
-                #self.marker_dict[int(id)]['corner_0'] = marker_corners[index][0]
-                #self.marker_dict[int(id)]['corner_1'] = marker_corners[index][1]
-                #self.marker_dict[int(id)]['corner_2'] = marker_corners[index][2]
-                #self.marker_dict[int(id)]['corner_3'] = marker_corners[index][3]
-
-
         # clear the dictionary 
         # marker_dict.clear()
                 
-
 
     # generate an empty frame and put the ID information into it
     def txt_image_generation(self):
@@ -167,12 +169,12 @@ class marker:
                     self.text_image = cv2.putText(id_background , display_text , org , font , fontScale , color , thickness , cv2.LINE_AA)
 
 
-    # get an particular id's mask
+    # get an particular id's mask image 
+    # just an testing script for testing all the holes in the marker
     def mask_test(self, id):
+
         # create an maske image
         mask = np.zeros(self.depth_image.shape , dtype = np.uint8)
-        
-        
         
         # check to see if marker is in the dictionary
         if id in self.marker_dict:
@@ -197,13 +199,164 @@ class marker:
             mask = cv2.bitwise_and(self.depth_image , self.depth_image , mask = mask)
 
             # apply color map for the mask image
-            mask= cv2.applyColorMap(cv2.convertScaleAbs(mask , alpha = 0.04) , cv2.COLORMAP_JET)
+            mask = cv2.applyColorMap(cv2.convertScaleAbs(mask , alpha = 0.04) , cv2.COLORMAP_JET)
+        
+        return mask
+    
+
+    # here is the function that will generate the mask image and also plot the histogram for the depth
+    # Base on the historgram it will tell us how much of the image have holes in it
+    def mask_hist_plot(self, id):
+    # create an maske image
+        mask = np.zeros(self.depth_image.shape , dtype = np.uint8)
+        
+        # check to see if marker is in the dictionary
+        if id in self.marker_dict:
+             
+            # import all the object into an np array 
+            marker_corner_0 = np.array(self.marker_dict[id]['corner_0'] , dtype = np.int32)
+            marker_corner_0 = np.expand_dims(marker_corner_0 , axis = 0)
+            marker_corner_1 = np.array(self.marker_dict[id]['corner_1'] , dtype = np.int32)
+            marker_corner_1 = np.expand_dims(marker_corner_1 , axis = 0)
+            marker_corner_2 = np.array(self.marker_dict[id]['corner_2'] , dtype = np.int32)
+            marker_corner_2 = np.expand_dims(marker_corner_2 , axis = 0)
+            marker_corner_3 = np.array(self.marker_dict[id]['corner_3'] , dtype = np.int32)
+            marker_corner_3 = np.expand_dims(marker_corner_3 , axis = 0)
+
+            # concatenate the numpy array as an single 2X4 array
+            marker_corner_np = np.concatenate((marker_corner_0 , marker_corner_1 , marker_corner_2 , marker_corner_3) , axis = 0)
+
+            # create an poly fill base on the marker corners
+            cv2.fillPoly(mask , [marker_corner_np] , 255)
+            
+            # ray casting algrotium to find all the pixel information within the bounding box
+
+
+
+            # roi depth mask for image
+            mask = cv2.bitwise_and(self.depth_image , self.depth_image , mask = mask)
+
+            # apply color map for the mask image
+            mask = cv2.applyColorMap(cv2.convertScaleAbs(mask , alpha = 0.04) , cv2.COLORMAP_JET)
         
         return mask
 
 
+    ###################################################
+    ###             Private Function                ###
+    ###################################################
+    ################################################################################################################################################
+
+    # ray casting function to check see if pixel is within the polygon
+    # looking for an specific aruco marker in the marker dict
+    def ray_casting_check(self, point_x , point_y ,id):
+
+        # define max system variable
+        huge = sys.float_info.max
+
+        # we start from outside of the polygon            
+        # states variable declear
+        inside = False
+        
+        # error check make sure the ID we passed in is detected
+        if id in self.marker_dict:
+            
+
+            # import all the object into an np array 
+            marker_corner_0 = np.array(self.marker_dict[id]['corner_0'] , dtype = np.int32)
+            marker_corner_0 = np.expand_dims(marker_corner_0 , axis = 0)
+            marker_corner_1 = np.array(self.marker_dict[id]['corner_1'] , dtype = np.int32)
+            marker_corner_1 = np.expand_dims(marker_corner_1 , axis = 0)
+            marker_corner_2 = np.array(self.marker_dict[id]['corner_2'] , dtype = np.int32)
+            marker_corner_2 = np.expand_dims(marker_corner_2 , axis = 0)
+            marker_corner_3 = np.array(self.marker_dict[id]['corner_3'] , dtype = np.int32)
+            marker_corner_3 = np.expand_dims(marker_corner_3 , axis = 0)
+
+            # concatenate the numpy array as an single 2X4 array
+            marker_corner_np = np.concatenate((marker_corner_0 , marker_corner_1 , marker_corner_2 , marker_corner_3) , axis = 0)
+            
+            # pass corners into the sorting algorithms to get them in an rotation oriantation
+            marker_corner_np = self.__corner_sorting(marker_corner_np)
+            print(marker_corner_np)
+
+
+
+            # loop though all the corners
+            n = marker_corner_np.shape[0]
+
+            for i in range(n):
+
+                # import the first two point
+                p1x = marker_corner_np[i-1][0]
+                p1y = marker_corner_np[i-1][1]
+                p2x = marker_corner_np[i][0]
+                p2y = marker_corner_np[i][1]
+
+                # compare the y value to make sure p1y is the lower point
+                if p1y > p2y:
+                    p1x , p1y , p2x , p2y = p2x , p2y , p1x , p1y 
+                
+                # check if the point is the same height as p1y and p2y
+                #if point_y == p1y or point_y == p2y:
+                    
+                    # add an fraction on it to make sure when we calculate the intercept there will not be divided by 0
+                    #point_y = point_y + 0.001
+
+                # check intercept on the horiztal ray casting
+                if point_y > p1y and point_y < p2y:
+                    
+                    # this means it intercept with the first edge
+                    inside = not inside
+                    
+                    # check the slope to make up the odd even times that the ray passing by
+                    # we default these to max because the slope of an function that is parallel to the y axis is infinity
+                    try: 
+                        m_edge = (p2y - p1y) / (p2x - p1x)
+                    except:
+                        m_edge = huge
+
+                    try:
+                        m_point = (point_y - p1y) / (point_x - p1x)
+
+                    except:
+                        m_point = huge
+
+                    # check the slope to see we just need to make sure either one is bigger to get and odd number
+                    
+                    if m_point > m_edge:
+                        inside = not inside
+                        
+                    
+                        print("intercept with " , i-1 ,  i)
+
+                    
+
+            
+        else:
+            print("Marker not in class")
+
+
+        return inside
 
     
+    # sorting out corners orination
+    # We will calculate the centroid and check the angle for each point relative to centroid 
+    # Sort all the point base on angles
+    def __corner_sorting(self, corners):
+        
+        # calculate the centroid of the polygon
+        centriod = np.mean(corners, axis = 0)
+
+        # calculate the angle to each point relative to centroid
+        angles = np.arctan2(corners[:,1] - centriod[1] , corners[:,0] - centriod[0])
+
+        # sort points by angles
+        sort_index = np.argsort(angles)
+
+        # reindex the points and return them
+        return corners[sort_index]
+        
+
 
 
             
--- Realsense Playground/python_file/simple_marker_playground/main.py
+++ Realsense Playground/python_file/simple_marker_playground/main.py
@@ -7,12 +7,22 @@ marker_obj = marker()
 while True:
     marker_obj.get_frames()
     marker_obj.simple_marker_detection()
-    mask_img = marker_obj.mask_test(2)
+    mask_img = marker_obj.mask_test(1)
+    check_point = marker_obj.ray_casting_check(100 , 500, 1)
+    
+    # debuging  
+    if check_point == True:
+        cv2.circle(mask_img , (100,500) , 20 , (255, 255, 255) , 5)
+    else :
+        cv2.circle(mask_img , (100,500) , 20 , (0 , 0 , 0) , 5)
+
 
     merge_image = np.hstack((marker_obj.color_image , marker_obj.depth_colormap))
 
-    cv2.namedWindow('merge image' , cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('merge image' , mask_img)
+    print(check_point)
+
+    cv2.namedWindow('masked image' , cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('masked image' , mask_img)
     key = cv2.waitKey(1)
 
     if key & 0XFF == ord('q') or key == 27:
